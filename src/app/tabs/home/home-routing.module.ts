d547244077211bd526f99860f05802898cf1359c
Richard Langworthy <langersuk@hotmail.co.uk>
2020-05-31 14:45:24 +0100

    Routing

---
+++ src/app/tabs/home/home-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { HomePage } from "./home.page";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomePage,
+  },
+  {
+    path: "edit/:receiptId",
+    loadChildren: () =>
+      import("./edit/edit.module").then((m) => m.EditPageModule),
+  },
+  {
+    path: ":receiptId",
+    loadChildren: () =>
+      import("./detail/detail.module").then((m) => m.DetailPageModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HomePageRoutingModule {}
